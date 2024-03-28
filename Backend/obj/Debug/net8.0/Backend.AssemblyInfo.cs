805b759ed5d8a464dbb7bf9b4232756b4970f012
RodryJR <dariordguezll@gmail.com>
2024-03-28 01:06:24 -0400

    DTOs cliente and actor

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f8fae13a923deeb50e8058ee8f06f9dfbf6528")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71b4cdbc15cad2dccf4b39ff5746cc2357b30db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
