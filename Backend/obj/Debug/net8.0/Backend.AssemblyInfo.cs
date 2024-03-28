ca7303ce884d2919fd7b35f6887b832728382399
RodryJR <dariordguezll@gmail.com>
2024-03-27 23:14:22 -0400

    merge

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1912496602a6b53ca71a85484d4ddb9fe0b66c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1e05e8dcd95a21fd4fc04bfe89ee42ac9e942a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
