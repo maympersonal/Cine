c66974c23266486d64ace6e08b1ae28385cd5d55
RodryJR <dariordguezll@gmail.com>
2024-04-02 03:30:17 -0400

    cosas

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3bc60efedbb6d2330ef285277c7c044a09c14d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7305dc4d0786c5b07a01ca8575b00b762449a474")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
