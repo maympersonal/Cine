a5386cd3f8e86551476022359127068a94d4b5b2
Alejandrito <alejandrito110402@gmail.com>
2024-04-02 00:59:03 -0400

    aaaa

--- Frontend/src/components/MovieCard/MovieCard.jsx
+++ Frontend/src/components/MovieCard/MovieCard.jsx
@@ -1,44 +1,36 @@
-// Asegúrate de importar axios desde tu configuración de axios personalizada
-import axios from '../../api/axios';
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import movieNotFound from '/assets/img/movie-not-found.svg';
 
-const MovieCard = ({ id }) => {
-    const [movieDetails, setMovieDetails] = useState({});
-    const [genres, setGenres] = useState([]);
-    const [actors, setActors] = useState([]);
 
-    const fetchMovieDetails = async () => {
-        try {
-            const { data } = await axios.get(`Pelicula/GetById/${id}`);
-            setMovieDetails(data);
-            setGenres(data.idGs); // Directamente asigna los géneros ya que vienen como strings
-            setActors(data.idAs); // Directamente asigna los actores ya que vienen como strings
-        } catch (error) {
-            console.error('Error fetching movie details:', error);
-        }
-    };
+const MovieCard = ({
+    idP,
+    titulo,
+    sinopsis,
+    imagen,
+    duración,
+    idAs,
+    idGs
+}) => {
 
-    useEffect(() => {
-        fetchMovieDetails();
-    }, [id]);
+    const imgUrl = imagen ? imagen : movieNotFound;
 
-    const imgUrl = movieDetails.imagen ? movieDetails.imagen : movieNotFound;
+
+    const genres = idGs.join(', ');
+    const actors = idAs.join(', ');
 
     return (
         <div className="movieCard">
             <div className="movieCard_img">
-                <img src={imgUrl} alt={`Poster of the movie '${movieDetails.titulo}'`} className={!movieDetails.imagen ? 'movieCard_img_notFound' : ''} />
+                <img src={imgUrl} alt={`Poster de la película '${titulo}'`} className={!imagen ? 'movieCard_img_notFound' : ''} />
             </div>
             <div className="movieCard_info">
-                <h2 className="movieCard_info__title uppercase">{movieDetails.titulo}</h2>
-                <p>{movieDetails.sinopsis || 'No synopsis available.'}</p>
-                <div>Duration: {movieDetails.duración} minutes</div>
-                <div>Genre(s): {genres.join(', ')}</div>
-                <div>Cast: {actors.join(', ')}</div>
-                <Link to={`/movie/${id}`}>
-                    <button className="btn btn-sm btn-primary rounded uppercase movieCard_extraInfo_verFicha">View Full Details</button>
+                <h2 className="movieCard_info__title uppercase">{titulo}</h2>
+                <p>{sinopsis || 'No hay información sobre la sinopsis de esta película.'}</p>
+                <div>Duración: {duración} minutos</div>
+                <div>Género(s): {genres}</div>
+                <div>Elenco: {actors}</div>
+                <Link to={`/movie/${idP}`}>
+                    <button className="btn btn-sm btn-primary rounded uppercase movieCard_extraInfo_verFicha">Ver ficha completa</button>
                 </Link>
             </div>
         </div>
