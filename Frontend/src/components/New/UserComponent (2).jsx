07daa5d00fdb9950e7df8ce5fc897b6c22310f12
Artagos <cartayaarnaldo4@gmail.com>
2024-04-02 04:02:03 -0400

    commitcito

--- Frontend/src/components/New/UserComponent (2).jsx
+++ Frontend/src/components/New/UserComponent (2).jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import './UserComponent (2).css'; // Asegúrate de tener el archivo CSS para el estilo
+import axios from '../../api/axios';
 
-const ClienteCard = ({ nombre, estadoInicial }) => {
+const ClienteCard = ({ nombre, estadoInicial ,ci,correo}) => {
   const [estadoConfianza, setEstadoConfianza] = useState(estadoInicial);
   const [eliminado, setEliminado] = useState(false);
-
-  const cambiarEstadoConfianza = () => {
+  const [update,setUpdate]=useState({Ci:ci,Correo:correo,Confiabilidad:!estadoInicial})
+  const cambiarEstadoConfianza = async() => {
+    await axios.put('/Cliente/Update/'+ci,update)
+    .then(res=>{console.log(res.status)})
+    .catch(error=>console.log(error+"    ayayayayayayaya"))
     setEstadoConfianza(!estadoConfianza);
+    setUpdate({Ci:ci,Correo:correo,Confiabilidad:!estadoConfianza})
+
   };
 
   const handleEliminar = () => {
